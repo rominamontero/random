af053ef0a3d27e803f30b39b51c82840446737b5
rominamontero <rmontero2208@gmail.com>
2026-04-27 10:54:48 -0400

    yans

--- numerito.py
+++ numerito.py
@@ -3,13 +3,18 @@ import random
 
 num=random.randint(1,100)
 g=int(input("Adivina el numero: "))
+pos=1
 
 
-
-while num != g:
+while pos <5 and g!=num:
+    print(f"Turno {pos}")
     if g>num:
         print("Te pasaste")
     else:
         print("El numero a adivinar es mayor")
     g=int(input("Adivina el numero: "))
-print("Has adivinado")
+    pos+=1
+if g==num:
+    print("Gano")
+else:
+    print("Peldio. Se te acabaron las oportunidades, el numero a adivinar era: ",num)
