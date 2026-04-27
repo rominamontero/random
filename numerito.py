4c9cc6d1a53866ff1450a969aab2056e8e228f18
rominamontero <rmontero2208@gmail.com>
2026-04-27 11:07:07 -0400

    randomktm

--- numerito.py
+++ numerito.py
@@ -5,16 +5,21 @@ num=random.randint(1,100)
 g=int(input("Adivina el numero: "))
 pos=1
 
+while g<1 or g >100:
+    print("Numero fuera de rango, intente nuevamente")
 
-while pos <5 and g!=num:
-    print(f"Turno {pos}")
-    if g>num:
-        print("Te pasaste")
+    while pos <5 and g!=num:
+        g=int(input("Adivina el numero: "))
+        print(f"Turno {pos}")
+        if g>num:
+            print("Te pasaste")
+        else:
+            print("El numero a adivinar es mayor")
+        g=int(input("Adivina el numero: "))
+        while g<=1 and g >=100:
+            print("Numero fuera de rango, intente nuevamente")
+        pos+=1
+    if g==num:
+        print("Gano")
     else:
-        print("El numero a adivinar es mayor")
-    g=int(input("Adivina el numero: "))
-    pos+=1
-if g==num:
-    print("Gano")
-else:
-    print("Peldio. Se te acabaron las oportunidades, el numero a adivinar era: ",num)
+        print("Peldio. Se te acabaron las oportunidades, el numero a adivinar era: ",num)
